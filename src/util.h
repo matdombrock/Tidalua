dd43dc8fb74bbbcf90a7e4d5c1a035b2e505b012
matdombrock <dombrock.mathieu@gmail.com>
2025-03-17 00:52:58 -0700

    cleanup util

--- src/util.h
+++ src/util.h
@@ -1,14 +1,7 @@
 #ifndef UTIL_H
 #define UTIL_H
 #include <stdio.h>
-#include <math.h>
-#include <portaudio.h>
-#include <string.h>
-// There is obviously some bullshit going on here
-// These lua includes should not be needed
-#include "../lua/src/lua.h"
-#include "../lua/src/lauxlib.h"
-#include "../lua/src/lualib.h"
+#include <stdarg.h>
 #include "globals.h"
 
 void debug(const char *format, ...) {
