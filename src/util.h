c9e49359303e8163437aa3a913869b972edc6bfe
matdombrock <dombrock.mathieu@gmail.com>
2025-03-17 00:32:54 -0700

    cleanup

--- src/util.h
+++ src/util.h
@@ -12,12 +12,11 @@
 #include "globals.h"
 
 void debug(const char *format, ...) {
-    if (_sys.output_mode == 1) {
-        va_list args;
-        va_start(args, format);
-        vprintf(format, args);
-        va_end(args);
-    }
+    if (_sys.output_mode != 1) return;
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
 }
 
 #endif // UTIL_H
